e633890ea23295e9dd1133243dd123910b29d225
electromop <user4567859@gmail.com>
2023-03-03 19:11:24 +0300

    ..

--- hurr-app-site/static/scripts/graphics.js
+++ hurr-app-site/static/scripts/graphics.js
@@ -1,39 +1,17 @@
 Chart.defaults.global.legend.display = false;
 var ctx = document.getElementById("myChart");
 
-let ethPrice_1 = []; // массив для хранения курса ефириума
-let updateTimes_1 = []; // массив для хранения времени обновления курса
+let ethPrice = [23223, 2312121, 32322, 34435, 55342, 5342313, 23424]; // массив для хранения курса ефириума
+let updateTimes = []; // массив для хранения времени обновления курса
 
-function updateEthPrice() {
-  // делаем запрос на API для получения текущего курса ефириума
-  fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
-    .then(response => response.json())
-    .then(data => {
-      // добавляем текущую цену в массив ethPrice_1
-      ethPrice_1.push(data.ethereum.usd);
-      // добавляем текущее время в массив updateTimes_1
-      updateTimes_1.push(new Date().toLocaleString());
-      while (ethPrice_1.length > 10) and (updateTimes_1.length > 10) 
-      {
-            ethPrice_1.shift();
-            updateTimes_1.shift();
-      }
-      console.log('Eth price updated:', ethPrice_1, updateTimes_1);
-
-    });
-}
-
-// вызываем функцию updateEthPrice каждый час
-setInterval(updateEthPrice, 60 * 60 * 1000);
-
-var myChart = new Chart(ctx, {
+const newLocal = new Chart(ctx, {
   type: "line",
   data: {
-    labels: updateTimes_1,
+    labels: updateTimes,
     datasets: [
       {
         label: "Hurricane Price",
-        data: ethPrice_1,
+        data: ethPrice,
         backgroundColor: "RGBA(255,145,0,1)",
         borderColor: "RGBA(255,145,0,1)",
         color: '#FFFFFF',
@@ -44,3 +22,21 @@ var myChart = new Chart(ctx, {
     ]
   }
 });
+function updateEthPrice() {
+  fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
+    .then(response => response.json())
+    .then(data => {
+      if (ethPrice.length == 10)
+      {
+            ethPrice.shift();
+            updateTimes.shift();
+      }
+      ethPrice.push(Object.values(data));
+      updateTimes.push(Object.keys(data)[0].slice(10));
+      console.log('Eth price updated:', ethPrice, updateTimes);
+    });
+    var myChart = newLocal;
+}
+
+// вызываем функцию updateEthPrice каждый час
+setInterval(updateEthPrice, 60 * 1000);
---
+++ hurr-app-site/static/scripts/graphics1.js
@@ -0,0 +1,21 @@
+Chart.defaults.global.legend.display = false;
+var ctx = document.getElementById("myChart");
+
+const myChart1 = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: updateTimes,
+      datasets: [
+        {
+          label: "Hurricane Price",
+          data: ethPrice,
+          backgroundColor: "RGBA(255,145,0,1)",
+          borderColor: "RGBA(255,145,0,1)",
+          color: '#FFFFFF',
+          borderWidth: 1,
+          fill: false,
+          lineTension: 0.3
+        }
+      ]
+    }
+  });
