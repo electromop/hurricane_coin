912cafe39094446967869b5d7581c61eaaffc959
lowbrains <126882213+lowbrains@users.noreply.github.com>
2023-03-03 18:22:29 +0300

    Я СДЕЛАЛ ГРАФИКИ!!!!

--- hurr-app-site/static/scripts/graphics.js
+++ hurr-app-site/static/scripts/graphics.js
@@ -1,46 +1,46 @@
-Chart.defaults.global.legend.display = false;
-var ctx = document.getElementById("myChart");
+// Chart.defaults.global.legend.display = false;
+// var ctx = document.getElementById("myChart");
 
-let ethPrice_1 = []; // массив для хранения курса ефириума
-let updateTimes_1 = []; // массив для хранения времени обновления курса
+// let ethPrice = []; // массив для хранения курса ефириума
+// let updateTimes = []; // массив для хранения времени обновления курса
 
-function updateEthPrice() {
-  // делаем запрос на API для получения текущего курса ефириума
-  fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
-    .then(response => response.json())
-    .then(data => {
-      // добавляем текущую цену в массив ethPrice_1
-      ethPrice_1.push(data.ethereum.usd);
-      // добавляем текущее время в массив updateTimes_1
-      updateTimes_1.push(new Date().toLocaleString());
-      while (ethPrice_1.length > 10) and (updateTimes_1.length > 10) 
-      {
-            ethPrice_1.shift();
-            updateTimes_1.shift();
-      }
-      console.log('Eth price updated:', ethPrice_1, updateTimes_1);
+// function updateEthPrice() {
+//   // делаем запрос на API для получения текущего курса ефириума
+//   fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
+//     .then(response => response.json())
+//     .then(data => {
+//       // добавляем текущую цену в массив ethPrice
+//       if (ethPrice.length == 10) {
+//         if(updateTimes.length == 10) {
+//           ethPrice.shift();
+//           updateTimes.shift();
+//         }
+//       }
+//       ethPrice.push(Object.values(data));
+//       // добавляем текущее время в массив updateTimes_1
+//       updateTimes.push(Object.keys(data));
+//       console.log('Eth price updated:', ethPrice, updateTimes);
+//   });
+// }
 
-    });
-}
+// // вызываем функцию updateEthPrice каждый час
+// setInterval(updateEthPrice, 5 * 1000);
 
-// вызываем функцию updateEthPrice каждый час
-setInterval(updateEthPrice, 60 * 60 * 1000);
-
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: updateTimes_1,
-    datasets: [
-      {
-        label: "Hurricane Price",
-        data: ethPrice_1,
-        backgroundColor: "RGBA(255,145,0,1)",
-        borderColor: "RGBA(255,145,0,1)",
-        color: '#FFFFFF',
-        borderWidth: 1,
-        fill: false,
-        lineTension: 0.3
-      }
-    ]
-  }
-});
+// var myChart = new Chart(ctx, {
+//   type: "line",
+//   data: {
+//     labels: updateTimes,
+//     datasets: [
+//       {
+//         label: "Hurricane Price",
+//         data: ethPrice,
+//         backgroundColor: "RGBA(255,145,0,1)",
+//         borderColor: "RGBA(255,145,0,1)",
+//         color: '#FFFFFF',
+//         borderWidth: 1,
+//         fill: false,
+//         lineTension: 0.3
+//       }
+//     ]
+//   }
+// });
