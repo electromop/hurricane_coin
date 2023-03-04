713c9c81257b806beb91d0df64cec79b11e943cb
electromop <user4567859@gmail.com>
2023-03-04 18:47:08 +0300

    added corousel to index

--- hurr-app-site/static/scripts/graph_data.js
+++ hurr-app-site/static/scripts/graph_data.js
@@ -1,20 +1,27 @@
+// let ethPrice = [];
+// let updateTimes = [];
+
+// let ethPrice_1 = [];
+// let updateTimes_1 = [];
 
 
-function updateEthPrice() {
-    fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
-      .then(response => response.json())
-      .then(data => {
-        if (ethPrice.length == 10)
-        {
-              ethPrice.shift();
-              updateTimes.shift();
-        }
-        ethPrice.push(Object.values(data));
-        updateTimes.push(Object.keys(data)[0].slice(10));
-        console.log('Eth price updated:', ethPrice, updateTimes);
-      });
-      var myChart = newLocal;
-  }
+// function updateEthPrice() {
+//     fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
+//       .then(response => response.json())
+//       .then(data => {
+//         if (ethPrice.length == 10)
+//         {
+//               ethPrice.shift();
+//               updateTimes.shift();
+//         }
+//         ethPrice.push(Object.values(data));
+//         updateTimes.push(Object.keys(data)[0].slice(10));
+//         console.log('myChart updated:', ethPrice, updateTimes);
+//       });
+//     ethPrice_1 = ethPrice;
+//     updateTimes_1 = updateTimes;
+//     console.log('myChart_1 updated:', ethPrice_1, updateTimes_1)
+//   }
   
-  // вызываем функцию updateEthPrice каждый час
-  setInterval(updateEthPrice, 60 * 1000);
+//   // вызываем функцию updateEthPrice каждый час
+//   setInterval(updateEthPrice, 60 * 1000);
--- hurr-app-site/static/scripts/graphics.js
+++ hurr-app-site/static/scripts/graphics.js
@@ -1,42 +1,42 @@
 Chart.defaults.global.legend.display = false;
 var ctx = document.getElementById("myChart");
 
-let ethPrice = [23223, 2312121, 32322, 34435, 55342, 5342313, 23424]; // массив для хранения курса ефириума
-let updateTimes = []; // массив для хранения времени обновления курса
+let ethPrice = [];
+let updateTimes = [];
+const myChart = newChart(ctx, {});
 
-const newLocal = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: updateTimes,
-    datasets: [
-      {
-        label: "Hurricane Price",
-        data: ethPrice,
-        backgroundColor: "RGBA(255,145,0,1)",
-        borderColor: "RGBA(255,145,0,1)",
-        color: '#FFFFFF',
-        borderWidth: 1,
-        fill: false,
-        lineTension: 0.3
-      }
-    ]
-  }
-});
 function updateEthPrice() {
-  fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
-    .then(response => response.json())
-    .then(data => {
-      if (ethPrice.length == 10)
-      {
-            ethPrice.shift();
-            updateTimes.shift();
-      }
-      ethPrice.push(Object.values(data));
-      updateTimes.push(Object.keys(data)[0].slice(10));
-      console.log('Eth price updated:', ethPrice, updateTimes);
-    });
-    var myChart = newLocal;
-}
-
+    fetch('http://127.0.0.1:5000/hurricane/api/v1.0/hurr-rate/last-rate')
+      .then(response => response.json())
+      .then(data => {
+        if (ethPrice.length == 10)
+        {
+              ethPrice.shift();
+              updateTimes.shift();
+        }
+        ethPrice.push(Object.values(data));
+        updateTimes.push(Object.keys(data)[0].slice(10));
+        console.log('myChart updated:', ethPrice, updateTimes);
+      });
+      myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: updateTimes,
+          datasets: [
+            {
+              label: "Hurricane Price",
+              data: ethPrice,
+              backgroundColor: "RGBA(255,145,0,1)",
+              borderColor: "RGBA(255,145,0,1)",
+              color: '#FFFFFF',
+              borderWidth: 1,
+              fill: false,
+              lineTension: 0.3
+            }
+          ]
+        }
+      });
+  }
+  
 // вызываем функцию updateEthPrice каждый час
 setInterval(updateEthPrice, 60 * 1000);
