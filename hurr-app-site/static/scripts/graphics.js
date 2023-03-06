1c3f1000c2543e444e7dfcb578fce2757831b84d
lowbrains <126882213+lowbrains@users.noreply.github.com>
2023-03-06 21:11:55 +0300

    Trying without data_collections

--- hurr-app-site/static/scripts/graphics.js
+++ hurr-app-site/static/scripts/graphics.js
@@ -1,14 +1,17 @@
 // Chart.defaults.global.legend.display = false;
 var ctx = document.getElementById("myChart");
 
+var mine = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var mine_1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o'];
+
   var myChart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: data_Collection_1,
+      labels: mine,
       datasets: [
         {
           label: "Hurricane Price",
-          data: data_Collection,
+          data: mine_1,
           backgroundColor: "RGBA(255,145,0,1)",
           borderColor: "RGBA(255,145,0,1)",
           color: '#FFFFFF',
