595368e5d5c0fd8a808921ceab1a59cf71f42416
lowbrains <126882213+lowbrains@users.noreply.github.com>
2023-04-24 20:35:10 +0300

    еще раз сделаешь асинхронную функцию, глаза вырву, графики готовы!

--- hurr-app-site/static/scripts/graph_data.js
+++ hurr-app-site/static/scripts/graph_data.js
@@ -5,23 +5,26 @@ var ethTime_1 = [];
 
 function getEthData() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://127.0.0.1:8090/hurricane/api/v1.0/hurr-rate/last-rate', true);
-  xhr.onload = function() {
+  xhr.open('GET', 'http://127.0.0.1:8090/hurricane/api/v1.0/hurr-rate/last-rate', false);
+  xhr.send();
+  // xhr.onload = function() {
     if (xhr.status === 200) {
       const data = JSON.parse(xhr.responseText);
       ethPrice = Object.values(data);
       ethTime = Object.keys(data);
-      ethPrice_1 = Object.values(data);
-      ethTime_1 = Object.keys(data);
+      // ethPrice_1 = Object.values(data);
+      // ethTime_1 = Object.keys(data);
       console.log(`Current ETH price: $${ethPrice}`);
       console.log(`Current ETH time: $${ethTime}`);
     } else {
       console.error('Error while fetching ETH price');
-    }
-  };
-  xhr.send();
+    // }
+  }
+  
+  return [ethPrice, ethTime];
+  // xhr.send();
 }
 
-getEthData();
+var [first, second] = getEthData();
 
 setInterval(getEthData, 11 * 60 * 1000);
